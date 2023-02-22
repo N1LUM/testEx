45a03307e1f85d0a3ae0ffe17baacdbd39e8d18a
N1LUM <serkipst678@gmail.com>
2023-02-22 22:11:23 +0300

    Da

---
+++ sender.php
@@ -0,0 +1,19 @@
+<?php
+	$phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $text = $_POST['text'];
+
+	$to = "borisivpavel219@gmail.com"; 
+	$date = date ("d.m.Y"); 
+	$time = date ("h:i");
+	$from = $email;
+	$subject = "Заявка c сайта";
+
+	
+	$msg="
+    Телефон: $phone /n
+    Почта: $email /n
+    Текст: $text"; 	
+	mail($to, $subject, $msg, "From: $from ");
+
+?>
